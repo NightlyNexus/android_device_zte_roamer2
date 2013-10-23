02795e33f695a09e5d939c220eb99cb5f595cc7a
Eric <eric@eric-tm2.(none)>
2013-10-23 00:09:45 -0400

    still not working

--- cm.mk
+++ cm.mk
@@ -1,8 +1,12 @@
 ## Specify phone tech before including full_phone
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 480
+TARGET_SCREEN_WIDTH := 320
+
 # Release name
-PRODUCT_RELEASE_NAME := roamer2
+PRODUCT_RELEASE_NAME := Open
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -16,3 +20,5 @@ PRODUCT_NAME := cm_roamer2
 PRODUCT_BRAND := zte
 PRODUCT_MODEL := roamer2
 PRODUCT_MANUFACTURER := zte
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=roamer2 BUILD_FINGERPRINT=zte/roamer2/m4:4.0.3/IMM76L/zteroamer2-V10a.20131022.170544:user/release-keys PRIVATE_BUILD_DESC="m4_open_eu-user 4.0.4 IMM76L zteroamer2-V10a.20131022.170544 release-keys"
